b12b3f21365b0ef8c38293f87c757d19d28e9c1d
Sage Markwardt <sagemarkwardt@gmail.com>
2024-05-30 16:42:05 -0400

    added submit button and some form stuff

--- form.php
+++ form.php
@@ -1,30 +1,31 @@
 <?php
-$path = $_SERVER['DOCUMENT_ROOT'].'/../config.php';
-require_once $path;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $path = $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
+    require_once $path;
 
-try {
-    // instantiate the PDO database Object
-    $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-    echo 'connect';
-} catch (PDOException $e){
-    // terminate if it can't connect to database
-    die($e->getMessage());
+    try {
+        // instantiate the PDO database Object
+        $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        echo 'connect';
+    } catch (PDOException $e) {
+        // terminate if it can't connect to database
+        die($e->getMessage());
+    }
+
+    $sql = "INSERT INTO student (last, first, GPA, SID, advisor, birthdate) 
+        VALUES (:last, :first, :GPA, :SID, :Advisor, :birthdate)";
+
+    $statement = $dbh->prepare($sql);
+    $statement->bindParam(':last', $last);
+    $statement->bindParam(':first', $first);
+
+    $statement->bindParam(':SID', $SID);
+    $statement->bindParam(':Birthdate', $Birthdate);
+
+    $statement->bindParam(':GPA', $GPA);
+    $statement->bindParam(':Advisor', $Advisor);
+
 }
-
-$sql = "INSERT INTO student (last, first) VALUES (:last, :first)";
-
-$statement = $dbh->prepare($sql);
-$statement->bindParam(':last', $last);
-$statement->bindParam(':first', $first);
-
-$statement->bindParam(':SID', $SID);
-$statement->bindParam(':Birthdate', $Birthdate);
-
-$statement->bindParam(':GPA', $GPA);
-$statement->bindParam(':Advisor', $Advisor);
-
-
-
 
 ?>
 <!doctype html>
@@ -38,29 +39,38 @@ $statement->bindParam(':Advisor', $Advisor);
 </head>
 <body>
 
-<h1> New students</h1>
-<form>
+<h1>New students</h1>
+    <form method = "post">
 
-<label for="sid"> ID:</label>
-<input type="text" sid="id" name="sid" required>
+        <label for="sid"> ID:</label>
+        <input type="text" sid="id" name="sid" required>
+        <br>
 
 
-<label for="last">Last Name:</label>
-<input type="text" id="last" name="last" required>
+        <label for="last">Last Name:</label>
+        <input type="text" id="last" name="last" required>
+        <br>
 
-<label for="first">First Name:</label>
-<input type="text" id="first" name="first" required>
+        <label for="first">First Name:</label>
+        <input type="text" id="first" name="first" required>
+        <br>
 
-<label for="birthdate">Birthdate:</label>
-<input type="date" id="birthdate" name="birthdate" required>
+        <label for="birthdate">Birthdate:</label>
+        <input type="date" id="birthdate" name="birthdate" required>
+        <br>
 
-<input type="text" id="gpa" name="gpa" required>
+        <label for="gpa">GPA:</label>
+        <input type="text" id="gpa" name="gpa" required>
+        <br>
 
-<label for="advisor">Advisor:</label>
-<input type="text" id="advisor" name="advisor" required>
-<br>
+        <label for="advisor">Advisor:</label>
+        <input type="text" id="advisor" name="advisor" required>
+        <br>
 
-</form>
+        <button type = submit>Add</button>
+
+
+    </form>
 
 </body>
 </html>
