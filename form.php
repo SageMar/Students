c00dc033c848f92006c127c34e8a09f9902de034
cmatthews444 <cmatthews4278@gmail.com>
2024-05-30 16:27:27 -0400

    added starting form

--- form.php
+++ form.php
@@ -1,1 +1,35 @@
-//test
+<?php
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h1> New students</h1>
+
+<label for="sid"> ID:</label>
+<input type="text" sid="id" name="sid" required>
+
+
+<label for="last">Last Name:</label>
+<input type="text" id="last" name="last" required>
+
+<label for="first">First Name:</label>
+<input type="text" id="first" name="first" required>
+<br>
+
+</form>
+
+</body>
+</html>>
+
+
